4cc42dcd6f3271d85450952679c395a5f7d9838b
DavidH <david.hill87@outlook.com>
2023-09-14 22:26:23 +1000

    Error handling

--- src/main/java/org/application/CalendarOptionSelect.java
+++ src/main/java/org/application/CalendarOptionSelect.java
@@ -5,6 +5,7 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class CalendarOptionSelect {
@@ -19,19 +20,20 @@ public class CalendarOptionSelect {
     static void getCalendarList() throws IOException, InterruptedException {
         // System prompt for user input for championship year
         System.out.println("Input Championship Year (eg. 2021): ");
-        int calendar_year = calyearScanner.nextInt();
+        try {
+            int calendar_year = calyearScanner.nextInt();
 
 
-        // Declarations
-        final int FIRST_YEAR = 1950;
-        final int CURRENT_YEAR = 2022;
+            // Declarations
+            final int FIRST_YEAR = 1950;
+            final int CURRENT_YEAR = 2022;
 
-        // API circuits for year URL - Dynamic input for user to choose the championship year & round (required)
-        final String POSTS_QUAL_API_URL = ("http://ergast.com/api/f1/" + calendar_year);
+            // API circuits for year URL - Dynamic input for user to choose the championship year & round (required)
+            final String POSTS_QUAL_API_URL = ("http://ergast.com/api/f1/" + calendar_year);
 
-        if (calendar_year >= FIRST_YEAR && calendar_year<= CURRENT_YEAR) {
-            System.out.println("You chose championship year: " + calendar_year);
-            System.out.println("#---------------------Race Calendar for " + calendar_year + "-----------------------#");
+            if (calendar_year >= FIRST_YEAR && calendar_year <= CURRENT_YEAR) {
+                System.out.println("You chose championship year: " + calendar_year);
+                System.out.println("#---------------------Race Calendar for " + calendar_year + "-----------------------#");
 
                 // SEND HTTP REQUESTS
                 HttpClient client = HttpClient.newHttpClient();
@@ -49,5 +51,11 @@ public class CalendarOptionSelect {
                 System.out.println("Choose a year between " + FIRST_YEAR + " & " + CURRENT_YEAR);
                 getCalendarList();
             }
+        } catch (InputMismatchException e) {
+            System.out.println("Invalid input. Please enter a valid year (e.g., 2021).");
+            calyearScanner.nextLine(); // Consume the invalid input
+            return; // Exit the method or handle it accordingly
+
+        }
     }
 }
